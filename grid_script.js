6bb630647a3f5151bcfe8eb6210adeaad0df110f
Thomas Bui <thomasthienbui@gmail.com>
2021-05-09 15:17:26 +0700

    added network reset and other options

--- grid_script.js
+++ grid_script.js
@@ -1,4 +1,8 @@
-//time the update and draw functions to see where time is being spent
+//add a choose data button (set number of points to a fixed number 64 or so)
+//add a training loss graph that updates with updates (epochs vs loss)
+//graph gradients in each layer (use colors to show different layers - 3 different gradients layers)
+//what else is interesting to observe? Affine transformations, and non-linear activations on those affine transformations in higher dimensional space
+//  either of one layer or all layers
 
 function distance_squared(x1, y1, x2, y2) {
   let dy = y1 - y2;
@@ -6,11 +10,18 @@ function distance_squared(x1, y1, x2, y2) {
   return dy * dy + dx * dx;
 }
 
-function get_cursor_position(canvas, event) {
+function get_cursor_position(canvas) {
   const rect = canvas.getBoundingClientRect();
   const xpos = event.clientX - rect.left;
   const ypos = event.clientY - rect.top;
 
+  return [xpos, ypos];
+}
+
+function handle_click(canvas, event) {
+  const [xpos, ypos] = get_cursor_position(canvas);
+
+  //delete closest point
   if (event.altKey) {
     if (predictors.length > 0) {
       let closest_idx = 0;
@@ -25,7 +36,7 @@ function get_cursor_position(canvas, event) {
       predictors.splice(closest_idx, 1);
       labels.splice(closest_idx, 1);
     }
-  } else {
+  } else { //add point (color based on modification keys pressed)
     predictors.push([xpos, ypos, 1]);
     if (event.shiftKey) labels.push([0, 1, 0]);
     else if (event.ctrlKey) labels.push([0, 0, 1]);
@@ -38,8 +49,7 @@ function get_cursor_position(canvas, event) {
 
 function update(x, y, lr, reg) {
   
-  //[dW1, db1, dW2, db2, dW3, db3] = tf.tidy(() => {
-  [dW1, dW2, dW3] = tf.tidy(() => {
+  [W1, W2, W3] = tf.tidy(() => {
     
     const z1 = tf.matMul(x, W1);
     const a1 = tf.relu(z1);
@@ -64,13 +74,12 @@ function update(x, y, lr, reg) {
     const da1 = tf.mul(dz2, tf.step(z1));
     const dW1 = tf.mul(-lr, tf.matMul(x.transpose(), da1));
 
-    return [tf.add(dW1, tf.mul(reg, W1)), tf.add(dW2, tf.mul(reg, W2)), tf.add(dW3, tf.mul(reg, W3))]
+    return [
+              tf.add(W1, tf.add(dW1, tf.mul(reg, W1))), 
+              tf.add(W2, tf.add(dW2, tf.mul(reg, W2))),
+              tf.add(W3, tf.add(dW3, tf.mul(reg, W3)))
+           ]
   });
-
-
-  W1 = tf.add(W1, dW1);
-  W2 = tf.add(W2, dW2);
-  W3 = tf.add(W3, dW3);
 
 }
 
@@ -85,9 +94,9 @@ function evaluate(x, weights) {
 
 function draw() {
   tf.tidy(() => {
-    console.time('eval');
+    //console.time('eval');
     const preds = evaluate(normalize(coords), [W1, W2, W3]); //this takes a long time
-    console.timeEnd('eval');
+    //console.timeEnd('eval');
     const classes = tf.argMax(preds, axis=1).arraySync();
 
     draw_contour(classes, 0, 'tomato');
@@ -126,7 +135,7 @@ function draw_circle(hot_index, color) {
 
 function update_and_draw() {
   for(let i = 0; i < 1; i++) {
-    update(normalize(predictors), labels, 0.01, 0.003);
+    update(normalize(predictors), labels, lr, wd);
   }
   draw();
 }
@@ -137,11 +146,12 @@ const bs = 8;
 
 tf.setBackend('cpu');
 
-function spiral_data(point_count) {
-  const r = Array.from({length: point_count}, (x, i) => i/point_count * 220);
-  const t0 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + Math.random()*0.4); //~1/3 of circle
-  const t1 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + 2.1 + Math.random()*0.4); //~1/3 of circle
-  const t2 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + 4.2 + Math.random()*0.4); //~1/3 of circle
+function spiral_data(point_total) {
+  const point_count = point_total / 2
+  const r = Array.from({length: point_count}, (x, i) => i/point_count * 210 + 10); //making spirals start a little way from the center
+  const t0 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + Math.random()*0.5); //~1/3 of circle
+  const t1 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + 2.1 + Math.random()*0.5); //~1/3 of circle
+  const t2 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + 4.2 + Math.random()*0.5); //~1/3 of circle
   const predictors = [];
   const labels = []
   for (let i = 0; i < point_count; i++) {
@@ -165,17 +175,17 @@ function spiral_data(point_count) {
 }
 
 
-
-function circle_data() {
+function random_data(points) {
   const predictors = [];
   const labels = []
-  for (let i = 0; i < 64; i++) {
+  for (let i = 0; i < points; i++) {
     let x = Math.random() * 512;
     let y = Math.random() * 512;
     predictors.push([x, y, 1]);
-    if (Math.pow(x-256, 2) + Math.pow(y-256, 2) > Math.pow(240, 2)) {
+    const n = Math.random();
+    if (n < .33) {
       labels.push([1, 0, 0]);
-    }else if (Math.pow(x-256, 2) + Math.pow(y-256, 2) > Math.pow(150, 2)) {
+    }else if (n < .66) {
       labels.push([0, 1, 0]);
     }else{
       labels.push([0, 0, 1]);
@@ -184,11 +194,39 @@ function circle_data() {
   return [predictors, labels];
 }
 
-const [predictors, labels] = circle_data(100);
+function circle_data(points) {
+  const predictors = [];
+  const labels = []
+  for (let i = 0; i < points; i++) {
+    let x = Math.random() * 512;
+    let y = Math.random() * 512;
+    let rs = Math.pow(x-256, 2) + Math.pow(y-256, 2);
+    if (rs > Math.pow(240, 2)) {
+      labels.push([1, 0, 0]);
+      predictors.push([x, y, 1]);
+    }else if (rs > Math.pow(150, 2) && rs < Math.pow(210, 2)) {
+      labels.push([0, 1, 0]);
+      predictors.push([x, y, 1]);
+    }else if(rs < Math.pow(120, 2)){
+      labels.push([0, 0, 1]);
+      predictors.push([x, y, 1]);
+    }
+  }
+  return [predictors, labels];
+}
 
+const [predictors, labels] = spiral_data(128);
 
+function gaussian_init(input_layers) {
+  return 1.
+}
+//assume ReLU is used for activation
 function kaiming_init(input_layers) {
   return Math.sqrt(2/input_layers);
+}
+//assumes 0 derivative for activation of 1
+function xavier_init(input_layers) {
+  return Math.sqrt(1/input_layers);
 }
 
 function normalize(tensor) {
@@ -206,15 +244,46 @@ for (let row = 0; row < bh; row += bs) {
 }
 
 //making feed forward layers
-const h_layers = 8;
-const h2_layers = 16;
+const h_layers = 12;
+const h2_layers = 12;
+
+function reset_weights() {
+  lr = parseFloat(document.getElementById("lr").value);
+  wd = parseFloat(document.getElementById("wd").value);
+  w_init_name = document.getElementById("w_init").value;
+  w_init_fn = gaussian_init;
+  switch(w_init_name) {
+    case "gaussian":
+      w_init_fn = gaussian_init;
+      break;
+    case "xavier":
+      w_init_fn = xavier_init;
+      break;
+    case "kaiming":
+      w_init_fn = kaiming_init;
+      break;
+  }
+
+  [W1, W2, W3] = tf.tidy(() => {
+    let w1 = tf.mul(tf.randomNormal([2 + 1, h_layers]), 1.0);
+    let w2 = tf.mul(tf.randomNormal([h_layers, h2_layers]), w_init_fn(h_layers));
+    let w3 = tf.mul(tf.randomNormal([h2_layers, 3]), w_init_fn(h2_layers));
+    return [w1, w2, w3];
+  });
+
+}
+
+//init weights
 let W1 = tf.mul(tf.randomNormal([2 + 1, h_layers]), 1.0);
 let W2 = tf.mul(tf.randomNormal([h_layers, h2_layers]), kaiming_init(h_layers));
 let W3 = tf.mul(tf.randomNormal([h2_layers, 3]), kaiming_init(h2_layers));
 
+let lr = 0.001;
+let wd = 0.001;
+
 const canvas = document.getElementById("canvas");
 canvas.addEventListener('mousedown', function(e) {
-  get_cursor_position(canvas, e);
+  handle_click(canvas, e);
 });
 const context = canvas.getContext("2d");
 
