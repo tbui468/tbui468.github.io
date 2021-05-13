d0136e22b075dc519e194e7a537564e3f27574dc
Thomas Bui <thomasthienbui@gmail.com>
2021-05-13 21:13:23 +0700

    added second contour graph, and shrunk the other one

--- grid_script.js
+++ grid_script.js
@@ -45,38 +45,36 @@ function handle_click(canvas, event) {
 }
 
 
-function train(x, y, lr, reg) {
+function train_network(x, y, lr, reg, W) {
   
-  [W1, W2, W3, z] = tf.tidy(() => {
+  return tf.tidy(() => {
     
-    const z1 = tf.matMul(x, W1);
+    const z1 = tf.matMul(x, W[0]);
     const a1 = tf.relu(z1);
-    const z2 = tf.matMul(a1, W2);
+    const z2 = tf.matMul(a1, W[1]);
     const a2 = tf.relu(z2);
-    const z3 = tf.matMul(a2, W3);
+    const z3 = tf.matMul(a2, W[2]);
     const probs = tf.softmax(z3, axis=1)
    
     //backproping over neg. log softmax as a single unit - dl/dprobs is (p) for negative labels, and (p-1) for positive labels
     const dprobs = tf.mul(tf.add(probs, tf.neg(y)), 1.0/x.shape[0]);
     const dW3 = tf.mul(-lr, tf.matMul(a2.transpose(), dprobs));
-    const dz3 = tf.matMul(dprobs, W3.transpose());
+    const dz3 = tf.matMul(dprobs, W[2].transpose());
 
     const da2 = tf.mul(dz3, tf.step(z2));
     const dW2 = tf.mul(-lr, tf.matMul(a1.transpose(), da2));
-    const dz2 = tf.matMul(da2, W2.transpose());
+    const dz2 = tf.matMul(da2, W[1].transpose());
 
     const da1 = tf.mul(dz2, tf.step(z1));
     const dW1 = tf.mul(-lr, tf.matMul(x.transpose(), da1));
 
     return [
-              tf.add(W1, tf.add(dW1, tf.mul(reg, W1))), 
-              tf.add(W2, tf.add(dW2, tf.mul(reg, W2))),
-              tf.add(W3, tf.add(dW3, tf.mul(reg, W3))),
+              tf.add(W[0], tf.add(dW1, tf.mul(reg, W[0]))), 
+              tf.add(W[1], tf.add(dW2, tf.mul(reg, W[1]))),
+              tf.add(W[2], tf.add(dW3, tf.mul(reg, W[2]))),
               z3
            ]
   });
-
-  return z;
 
 }
 
@@ -103,6 +101,17 @@ function draw() {
     draw_point(contour_plot, 0, c_red);
     draw_point(contour_plot, 1, c_green);
     draw_point(contour_plot, 2, c_blue);
+
+    const preds_2 = evaluate(normalize(coords), [W1_2, W2_2, W3_2]); //this takes a long time
+    const classes_2 = tf.argMax(preds_2, axis=1).arraySync();
+
+    draw_contour(contour_plot2, classes_2, 0, c_red);
+    draw_contour(contour_plot2, classes_2, 1, c_green);
+    draw_contour(contour_plot2, classes_2, 2, c_blue);
+
+    draw_point(contour_plot2, 0, c_red);
+    draw_point(contour_plot2, 1, c_green);
+    draw_point(contour_plot2, 2, c_blue);
   });
 
   let l = train_losses.length
@@ -120,7 +129,7 @@ function draw_contour(canvas, classes, hot_index, color) {
   const context = canvas.getContext("2d");
   context.beginPath();
   context.fillStyle = color;
-  for (let s = 0; s < (bh/bs)*(bh/bs); s++) {
+  for (let s = 0; s < (canvas.height/bs)*(canvas.height/bs); s++) {
       if (classes[s] == hot_index) {
         context.rect(coords[s][0], coords[s][1], bs, bs);
       }
@@ -149,6 +158,8 @@ function draw_point(canvas, hot_index, color) {
 
 
 function update_and_draw() {
+
+  //split data into training and validation sets
   let x = [];
   let y = [];
   let valid_x = [];
@@ -163,31 +174,46 @@ function update_and_draw() {
     }
   }
 
+  //train network
   if (x.length > 0) {
-    const z_train = train(normalize(x), y, lr, wd);
+    let z_train = 0;
+    let z_train_2 = 0;
+    [W1, W2, W3, z_train] = train_network(normalize(x), y, lr, wd, [W1, W2, W3]);
+    [W1_2, W2_2, W3_2, z_train_2] = train_network(normalize(x), y, lr_2, wd_2, [W1_2, W2_2, W3_2]);
     const probs_train = tf.softmax(z_train, axis=1)
+    const probs_train_2 = tf.softmax(z_train_2, axis=1)
     if(epoch % 10 == 0) {
       train_losses.push(tf.mean(tf.mul(tf.neg(tf.log(probs_train)), tf.step(y))).arraySync());
       train_accs.push(tf.sum(tf.equal(tf.argMax(y, axis=1), tf.argMax(z_train, axis=1))).arraySync() / y.length * 100)
+      train_losses_2.push(tf.mean(tf.mul(tf.neg(tf.log(probs_train_2)), tf.step(y))).arraySync());
+      train_accs_2.push(tf.sum(tf.equal(tf.argMax(y, axis=1), tf.argMax(z_train_2, axis=1))).arraySync() / y.length * 100)
     }
   }else{
     if(epoch % 10 == 0) {
       train_losses.push(0);
       train_accs.push(0);
+      train_losses_2.push(0);
+      train_accs_2.push(0);
     }
   }
 
   if (valid_x.length > 0) {
     const z_valid = evaluate(normalize(valid_x), [W1, W2, W3]);
     const probs_valid = tf.softmax(z_valid, axis=1)
+    const z_valid_2 = evaluate(normalize(valid_x), [W1_2, W2_2, W3_2]);
+    const probs_valid_2 = tf.softmax(z_valid_2, axis=1)
     if(epoch % 10 == 0) {
       valid_losses.push(tf.mean(tf.mul(tf.neg(tf.log(probs_valid)), tf.step(valid_y))).arraySync());
       valid_accs.push(tf.sum(tf.equal(tf.argMax(valid_y, axis=1), tf.argMax(z_valid, axis=1))).arraySync() / valid_y.length * 100)
+      valid_losses_2.push(tf.mean(tf.mul(tf.neg(tf.log(probs_valid_2)), tf.step(valid_y))).arraySync());
+      valid_accs_2.push(tf.sum(tf.equal(tf.argMax(valid_y, axis=1), tf.argMax(z_valid_2, axis=1))).arraySync() / valid_y.length * 100)
     }
   }else{
     if(epoch % 10 == 0) {
       valid_losses.push(0);
       valid_accs.push(0);
+      valid_losses_2.push(0);
+      valid_accs_2.push(0);
     }
   }
   epoch += 1;
@@ -195,27 +221,27 @@ function update_and_draw() {
 }
 
 
-function spiral_data(point_total) {
+function spiral_data(point_total, cw, ch) {
   const point_count = point_total / 2
-  const r = Array.from({length: point_count}, (x, i) => i/point_count * 210 + 10); //making spirals start a little way from the center
+  const r = Array.from({length: point_count}, (x, i) => i/point_count * (cw * .4)); //making spirals start a little way from the center
   const t0 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + Math.random()*1.0); //~1/3 of circle
   const t1 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + 2.1 + Math.random()*1.0); //~1/3 of circle
   const t2 = Array.from({length: point_count}, (x, i) => i/point_count * 4 + 4.2 + Math.random()*1.0); //~1/3 of circle
   const predictors = [];
   const labels = []
   for (let i = 0; i < point_count; i++) {
-    let x = r[i]*Math.cos(t0[i]) + 256;
-    let y = r[i]*Math.sin(t0[i]) + 256;
+    let x = r[i]*Math.cos(t0[i]) + 0.5*cw;
+    let y = r[i]*Math.sin(t0[i]) + 0.5*cw;
     predictors.push([x, y, 1]);
     labels.push([1, 0, 0]);
 
-    let x2 = r[i]*Math.cos(t1[i]) + 256;
-    let y2 = r[i]*Math.sin(t1[i]) + 256;
+    let x2 = r[i]*Math.cos(t1[i]) + 0.5*cw;
+    let y2 = r[i]*Math.sin(t1[i]) + 0.5*cw;
     predictors.push([x2, y2, 1]);
     labels.push([0, 1, 0]);
 
-    let x3 = r[i]*Math.cos(t2[i]) + 256;
-    let y3 = r[i]*Math.sin(t2[i]) + 256;
+    let x3 = r[i]*Math.cos(t2[i]) + 0.5*cw;
+    let y3 = r[i]*Math.sin(t2[i]) + 0.5*cw;
     predictors.push([x3, y3, 1]);
     labels.push([0, 0, 1]);
   }
@@ -223,25 +249,13 @@ function spiral_data(point_total) {
   return [predictors, labels];
 }
 
-function basic_data() {
-  const predictors = [];
-  const labels = [];
 
-  predictors.push([256, 200, 1]);
-  labels.push([1, 0, 0]);
-  predictors.push([200, 300, 1]);
-  labels.push([0, 1, 0]);
-  predictors.push([300, 300, 1]);
-  labels.push([0, 0, 1]);
-  return [predictors, labels];
-}
-
-function random_data(points) {
+function random_data(points, cw, ch) {
   const predictors = [];
   const labels = []
   for (let i = 0; i < points; i++) {
-    let x = Math.random() * 512;
-    let y = Math.random() * 512;
+    let x = Math.random() * cw;
+    let y = Math.random() * ch;
     predictors.push([x, y, 1]);
     const n = Math.random();
     if (n < .33) {
@@ -255,20 +269,20 @@ function random_data(points) {
   return [predictors, labels];
 }
 
-function circle_data(points) {
+function circle_data(points, cw, ch) {
   const predictors = [];
   const labels = []
   for (let i = 0; i < points; i++) {
-    let x = Math.random() * 512;
-    let y = Math.random() * 512;
-    let rs = Math.pow(x-256, 2) + Math.pow(y-256, 2);
-    if (rs > Math.pow(240, 2)) {
+    let x = Math.random() * cw;
+    let y = Math.random() * ch;
+    let rs = Math.pow(x-(cw/2), 2) + Math.pow(y-(ch/2), 2);
+    if (rs > Math.pow(cw/2 - 15, 2)) {
       labels.push([1, 0, 0]);
       predictors.push([x, y, 1]);
-    }else if (rs > Math.pow(150, 2) && rs < Math.pow(210, 2)) {
+    }else if (rs > Math.pow(cw/2-50, 2)) {
       labels.push([0, 1, 0]);
       predictors.push([x, y, 1]);
-    }else if(rs < Math.pow(120, 2)){
+    }else{
       labels.push([0, 0, 1]);
       predictors.push([x, y, 1]);
     }
@@ -299,8 +313,11 @@ function normalize(tensor) {
 function html_set_lr() {
   lr = parseFloat(document.getElementById("lr").value);
 }
+function html_set_lr2() {
+  lr_2 = parseFloat(document.getElementById("lr_2").value);
+}
 
-function restart_network(lr) {
+function restart_network() {
   return tf.tidy(() => {
     let w1 = tf.mul(tf.randomNormal([2 + 1, h_layers]), 1.0);
     let w2 = tf.mul(tf.randomNormal([h_layers, h2_layers]), kaiming_init(h_layers));
@@ -310,14 +327,20 @@ function restart_network(lr) {
 }
 
 function html_restart_network() {
-  let lr = parseFloat(document.getElementById("lr").value);
+  html_set_lr();
+  html_set_lr2();
   epoch = 1;
   train_losses = [];
   train_accs = [];
   valid_losses = [];
   valid_accs = [];
+  train_losses_2 = [];
+  train_accs_2 = [];
+  valid_losses_2 = [];
+  valid_accs_2 = [];
   html_redraw_plot();
-  [W1, W2, W3] = restart_network(lr);
+  [W1, W2, W3] = restart_network();
+  [W1_2, W2_2, W3_2] = restart_network();
 }
 
 function html_redraw_plot() {
@@ -390,17 +413,14 @@ function generate_data(data_type, prob) {
   if (prob < 0) prob = 0
   if (prob > 1) prob = 1
   switch(data_type) {
-    case "basic_data":
-      [x, y] = basic_data();
-      break;
     case "random_data":
-      [x, y] = random_data(32);
+      [x, y] = random_data(8, canvas.width, canvas.height);
       break;
     case "circle_data":
-      [x, y] = circle_data(128);
+      [x, y] = circle_data(64, canvas.width, canvas.height);
       break;
     case "spiral_data":
-      [x, y] = spiral_data(128);
+      [x, y] = spiral_data(64, canvas.width, canvas.height);
       break;
   }
   const i = split_indices(x.length, prob);
@@ -430,18 +450,6 @@ function split_indices(l, p) {
 
 
 
-const bw = 512;
-const bh = 512;
-const bs = 8;
-
-//setting up contour plot coordinates
-let coords = [];
-for (let row = 0; row < bh; row += bs) {
-  for (let col = 0; col < bw; col += bs) {
-    coords.push([row, col, 1])
-  }
-}
-
 //making feed forward layers
 const h_layers = 12;
 const h2_layers = 12;
@@ -458,18 +466,41 @@ let W1 = tf.mul(tf.randomNormal([2 + 1, h_layers]), 1.0);
 let W2 = tf.mul(tf.randomNormal([h_layers, h2_layers]), kaiming_init(h_layers));
 let W3 = tf.mul(tf.randomNormal([h2_layers, 3]), kaiming_init(h2_layers));
 
+let W1_2 = tf.mul(tf.randomNormal([2 + 1, h_layers]), 1.0);
+let W2_2 = tf.mul(tf.randomNormal([h_layers, h2_layers]), kaiming_init(h_layers));
+let W3_2 = tf.mul(tf.randomNormal([h2_layers, 3]), kaiming_init(h2_layers));
+
 let lr = 0.001;
 let wd = 0.001;
+let lr_2 = 0.001;
+let wd_2 = 0.001;
 let epoch = 1;
 let train_losses = [];
 let train_accs = [];
 let valid_losses = [];
 let valid_accs = [];
+let train_losses_2 = [];
+let train_accs_2 = [];
+let valid_losses_2 = [];
+let valid_accs_2 = [];
 
 const contour_plot = document.getElementById("canvas");
 contour_plot.addEventListener('mousedown', function(e) {
   handle_click(contour_plot, e);
 });
+const contour_plot2 = document.getElementById("canvas2");
+contour_plot2.addEventListener('mousedown', function(e) {
+  handle_click(contour_plot2, e);
+});
+
+const bs = 4;
+
+let coords = [];
+for (let row = 0; row < contour_plot.height; row += bs) {
+  for (let col = 0; col < contour_plot.width; col += bs) {
+    coords.push([row, col, 1])
+  }
+}
 
 const loss_plot = document.getElementById("loss_plot");
 const acc_plot = document.getElementById("acc_plot");
